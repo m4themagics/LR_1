b936f4663a986f13a31aac755fc49ff46b2deace
Smorodina7777 <dima26031@yandex.ru>
2023-11-09 14:51:24 +0700

    code review 1

--- Exc.5/FirstProgram.java
+++ Exc.5/FirstProgram.java
@@ -2,16 +2,13 @@ import FirstPackage.*;
 
 class FirstClass {
 	 public static void main(String[] args) {
+		 //What mean o?
 		SecondClass o = new SecondClass();
-		int i, j;
-		for(i = 1; i <= 8; i++)
-		{
-			for(j = 1; j <= 8; j++)
-			{
+		for(int i = 1; i <= 8; i++) {
+			for(int j = 1; j <= 8; j++) {
 				o.setParam1(i);
 				o.setParam2(j);
-				System.out.print(o.Sum());
-				System.out.print(" ");
+				System.out.print(o.Sum()+" ");
 			}
 			System.out.println();
 		}
